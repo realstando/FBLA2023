15d8d1e1f591d94c7c00d2779f84493b06ecd07e
Ryan Wang <rryanwwang@gmail.com>
2023-12-28 17:52:44 -0800

    finished accordions

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 let accordions = document.querySelectorAll(".accordion");
 accordions.forEach((acco) => {
     acco.onclick = () => {
-        acco.classList.add("active");
+        acco.classList.toggle("active");
+        let panel = acco.lastElementChild;
+        if (panel.style.maxHeight) {
+            acco.querySelector(".accordion-heading").querySelector(".dropdown").innerText = "+";
+            panel.style.maxHeight = null;
+        }
+        else {
+            acco.querySelector(".accordion-heading").querySelector(".dropdown").innerText = "-";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
     };
 });
